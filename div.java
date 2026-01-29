983a18168ca6ed9fb7e215757fc0711ce56d1c28
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-01-30 00:47:06 +0530

    divide

---
+++ div.java
@@ -0,0 +1,9 @@
+public class div {
+    public static void main(String[]a)
+    {
+        float c = 4;
+        float b = 2;
+        float divide = c/b;
+        System.out.println(divide);
+    }
+}
