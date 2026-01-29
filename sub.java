4f949b7ee620c8f7640713b05d88a080d082cd42
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-01-30 00:31:39 +0530

    sub

---
+++ sub.java
@@ -0,0 +1,11 @@
+public class sub {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int sub = a - b;
+        System.out.println(sub);
+    }
+
+
+}
