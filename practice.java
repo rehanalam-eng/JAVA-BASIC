8e432be18697f5bc5c467ae9cfe110ef0b44afbe
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-04 11:12:34 +0530

    swapping of two numbers

---
+++ practice.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class practice
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        // Swapping using temp
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
