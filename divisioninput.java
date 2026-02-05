124c36ea1f63db7bdec8f867ceb5aa6203d14c3c
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-05 22:47:56 +0530

    performing division,taking input from user

---
+++ divisioninput.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+
+public class divisioninput {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int division = a/b;
+        System.out.println(" after division a/b="+division);
+    }
+}
