31bdc343c191cb81218e53bb45af2255cbf30604
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-05 22:52:57 +0530

    performing multiplication,taking input from user

---
+++ multinput.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+
+public class multinput {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        float a = sc.nextInt();
+        float b = sc.nextInt();
+        
+        float multiplication = a*b;
+        System.out.println(" after multiplication a*b="+multiplication);
+    }
+}
