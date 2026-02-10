215c5bde253c195952bd458d2d73617c73300c8c
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-10 10:38:58 +0530

    largest of two numbers

---
+++ largestoftwo.java
@@ -0,0 +1,13 @@
+class largestoftwo{
+    public static void main(String[]a) {
+     
+        int b=20;
+        int c=10;
+        if(b<=c)
+        {
+            System.out.println("c is greater ");
+        }else {
+            System.out.println("b ic greater ");
+        }
+    }
+}
