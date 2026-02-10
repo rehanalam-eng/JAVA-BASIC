02930f71b3aab61a4c8bc524f9c7a8ee58bde0f1
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-10 10:59:55 +0530

    drive or not

---
+++ driveornot.java
@@ -0,0 +1,7 @@
+public class driveornot{
+    public static void main(String[] args) {
+        int Age = 4;
+        String type=(Age>=18)?"Drive":"Not drive";
+        System.err.println(type);
+    }
+}
