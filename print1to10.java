742996e5bc5c1600bd39e695cceb1e74c8e0f3eb
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-11 01:48:39 +0530

    print hello world 10 time

---
+++ print1to10.java
@@ -0,0 +1,10 @@
+public class print1to10{
+    public static void main(String[] args) {
+        int counter = 1;
+        while(counter<=10){
+            System.out.println("hello world ");
+            counter++;
+        }
+
+    }
+}
