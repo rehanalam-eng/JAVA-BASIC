fd984eb696835733cf7e96c88f395b09378fbc24
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-18 17:16:59 +0530

    functions using parameters

---
+++ fn2.java
@@ -0,0 +1,11 @@
+public class fn2 {
+
+    static void add(int a, int b) {
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+
+    public static void main(String[] args) {
+        add(5, 3);
+    }
+}
