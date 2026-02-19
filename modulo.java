2c41ab1b7fea134a55a690646765766f90cc5b0e
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-19 10:22:59 +0530

    functions using parameters ,modulus

---
+++ modulo.java
@@ -0,0 +1,11 @@
+public class modulo {
+
+    static void mod(int a, int b) {
+        int modulo = a % b;
+        System.out.println("Modulus= " + modulo);
+    }
+
+    public static void main(String[] args) {
+        mod(7, 3);
+    }
+}
