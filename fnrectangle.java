426e4384b0907eb96ea20d4465b0b014340d02fb
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-20 00:04:34 +0530

    functions using parameters ,area of rectangle

---
+++ fnrectangle.java
@@ -0,0 +1,11 @@
+public class fnrectangle {
+
+    static void AreaOfRectangle(int l, int b) {
+        int area = l*b;
+        System.out.println("Area of rectangle = " + area);
+    }
+
+    public static void main(String[] args) {
+        AreaOfRectangle(7, 3);
+    }
+}
