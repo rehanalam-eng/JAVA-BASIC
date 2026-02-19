026e811c1050ec2615f707f061708b6d0a0c1d2a
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-19 10:16:09 +0530

    functions using parameters multiplication

---
+++ multfn.java
@@ -0,0 +1,11 @@
+public class multfn {
+
+    static void multiplication(int a, int b) {
+        int mult = a * b;
+        System.out.println("Multiplication = " + mult);
+    }
+
+    public static void main(String[] args) {
+        multiplication(7,3);
+    }
+}
