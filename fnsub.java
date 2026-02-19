4527fae472928a730ca1cc199f7a32195b344a4b
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-19 10:13:01 +0530

    functions using parameters

---
+++ fnsub.java
@@ -0,0 +1,11 @@
+public class fnsub{
+
+    static void sub(int a, int b) {
+        int sub = a - b;
+        System.out.println("Sub = " + sub);
+    }
+
+    public static void main(String[] args) {
+        sub(5, 3);
+    }
+}
