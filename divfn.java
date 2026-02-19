100bbf1b801d2ce615c0779d0d850bfcfc482829
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-19 10:19:59 +0530

    functions using parameters ,division

---
+++ divfn.java
@@ -0,0 +1,9 @@
+public class divfn {
+    public static void div(float a, float b){
+        float divide = a/b;
+        System.out.println("divide : " + divide);
+    }
+    public static void main(String[] args) {
+        div(5, 2);
+    }
+}
