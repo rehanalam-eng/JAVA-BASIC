f71ca8ebd8c691a4571fc9998f0853c93cb24e67
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-22 23:00:52 +0530

    functions using parameters ,Perimeter of square

---
+++ PerimeterSquare.java
@@ -0,0 +1,11 @@
+public class PerimeterSquare {
+
+    static void perimeter(int side) {
+        int p = 4 * side;
+        System.out.println("Perimeter of Square = " + p);
+    }
+
+    public static void main(String[] args) {
+        perimeter(5);
+    }
+}
