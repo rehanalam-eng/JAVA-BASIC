94e9ee52ebe263f0526ed950b85a5e36aebce454
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-23 22:08:16 +0530

    sum of natural number

---
+++ Sumnatural.java
@@ -0,0 +1,14 @@
+public class Sumnatural {
+
+    static int sum(int n) {
+        int s = 0;
+        for(int i=1;i<=n;i++){
+            s += i;
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum = " + sum(5));
+    }
+}
