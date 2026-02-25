734a7c9c19c773ca59f03b31067dfd51c0254282
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-25 09:27:57 +0530

    month names

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+
+    static void readFile() throws IOException {
+        FileReader f = new FileReader("abc.txt");
+    }
+
+    public static void main(String[] args) throws IOException {
+        readFile();
+    }
+}
