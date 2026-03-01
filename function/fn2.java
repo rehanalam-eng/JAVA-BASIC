551f8c367145f381b6b6db7f3085e70f90ecfa1a
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-03-01 20:46:34 +0530

    greet

---
+++ function/fn2.java
@@ -0,0 +1,11 @@
+public class fn2 {
+
+    static void greet() {
+        System.out.println("Hello  ");
+        System.out.println("I was waiting for you... since 0 and 1 were invented ");
+    }
+
+    public static void main(String[] args) {
+        greet();  
+    }
+}
