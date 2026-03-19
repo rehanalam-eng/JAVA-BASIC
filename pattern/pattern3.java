a144ed85e7bfef50a8099ed822733a9c3927a54c
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-03-19 22:56:52 +0530

    pattern printing

---
+++ pattern/pattern3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class pattern3 {
+
+    // Function to print pattern
+    public static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        printPattern(n);  // function call
+    }
+}
